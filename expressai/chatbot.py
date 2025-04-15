3f9d3af29618b20de070b8345b6f4a0c5e0fcc98
Jaymes Dec <jdec@franklinjc.org>
2025-04-15 17:35:52 -0400

    Fixing Image url for analyze image

--- expressai/chatbot.py
+++ expressai/chatbot.py
@@ -110,8 +110,10 @@ class Chatbot:
                         "content": [
                             {"type": "text", "text": prompt},
                             {
-                                "type": "image_url",
-                                "image_url": f"data:image/jpeg;base64,{base64_image}"
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{base64_image}"
+                            }
                             },
                         ]
                     }
